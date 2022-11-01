7151c4d232b70075b441e070c8500a8945fbaaa5
Jesse Bakker <jesse.bakker@kadaster.nl>
2022-11-01 09:31:09 +0100

    update config/css

--- config.js
+++ config.js
@@ -9,92 +9,92 @@ var respecConfig = {
     maxTocLevel: 3,
     editors: [
         {
-        name: "Arjen Santema"
+            name: "Arjen Santema"
         },
         {
-        name: "Kees Trautwein"
+            name: "Kees Trautwein"
         },
     ],
     authors: [
         {
-        name: "Jos van der Arend (Kennisnet)"
+            name: "Jos van der Arend (Kennisnet)"
         },
         {
-        name: "Jesse Bakker (Kadaster)"
+            name: "Jesse Bakker (Kadaster)"
         },
         {
-        name: "Koos Boersma (IHW)"
+            name: "Koos Boersma (IHW)"
         },
         {
-        name: "Marco Brattinga (Nationale Politie)"
+            name: "Marco Brattinga (Nationale Politie)"
         },
         {
-        name: "Linda van den Brink (Geonovum)"
+            name: "Linda van den Brink (Geonovum)"
         },
         {
-        name: "Pieter van Everdingen (PLDN)"
+            name: "Pieter van Everdingen (PLDN)"
         },
         {
-        name: "Roeland Euwens (KNB)"
+            name: "Roeland Euwens (KNB)"
         },
         {
-        name: "Casper Gras (KOOP)"
+            name: "Casper Gras (KOOP)"
         },
         {
-        name: "Gerald Groot Roessink (DUO)"
+            name: "Gerald Groot Roessink (DUO)"
         },
         {
-        name: "Pano Maria (Skemu)"
+            name: "Pano Maria (Skemu)"
         },
         {
-        name: "Hans Overbeek (KOOP)"
+            name: "Hans Overbeek (KOOP)"
         },
         {
-        name: "Rik (CROW)"
+            name: "Rik (CROW)"
         },
         {
-        name: "Viyanti Ramdhiansing (Kennisnet)"
+            name: "Viyanti Ramdhiansing (Kennisnet)"
         },
         {
-        name: "Arjen Santema (Kadaster)"
+            name: "Arjen Santema (Kadaster)"
         },
         {
-        name: "Frank Terpstra (Geonovum)"
+            name: "Frank Terpstra (Geonovum)"
         },
         {
-        name: "Kees Trautwein (Logius)"
+            name: "Kees Trautwein (Logius)"
         },
         {
-        name: "Jan Willem van Veen (ArchiXL)"
+            name: "Jan Willem van Veen (ArchiXL)"
         },
     ],
     github: {
         branch: "main",
         repoURL: "pldn/nederlands-profiel-voor-stelselcatalogi",
     },
-//  localBiblio: lokale bibliografie, voor verwijzigingen
-//  NB: kijk eerst naar de beschikbare www.specref.org voor verwijziging
+    //  localBiblio: lokale bibliografie, voor verwijzigingen
+    //  NB: kijk eerst naar de beschikbare www.specref.org voor verwijziging
     localBiblio: {
         NLAPI: {
-        title: "API Strategie Algemeen (Nederlandse API Strategie I)",
-        href: "https://docs.geostandaarden.nl/api/API-Strategie/",
-        status: "Vastgesteld",
-        date: "09 maart 2022",
-        publisher: "Geonovum",
+            title: "API Strategie Algemeen (Nederlandse API Strategie I)",
+            href: "https://docs.geostandaarden.nl/api/API-Strategie/",
+            status: "Vastgesteld",
+            date: "09 maart 2022",
+            publisher: "Geonovum",
         },
         B1: {
-        title: "Taalniveau B1",
-        href: "https://www.communicatierijk.nl/vakkennis/rijkswebsites/aanbevolen-richtlijnen/taalniveau-b1",
+            title: "Taalniveau B1",
+            href: "https://www.communicatierijk.nl/vakkennis/rijkswebsites/aanbevolen-richtlijnen/taalniveau-b1",
         },
         PTOLULijst: {
-        title: "Pas-Toe-Of-Leg-Uit lijst van open standaarden",
-        href: "https://www.forumstandaardisatie.nl/open-standaarden/verplicht",
-        publisher: "Forum Standaardisatie",
+            title: "Pas-Toe-Of-Leg-Uit lijst van open standaarden",
+            href: "https://www.forumstandaardisatie.nl/open-standaarden/verplicht",
+            publisher: "Forum Standaardisatie",
         },
         PTOLU: {
-        title: "pas-toe-of-leg-uit",
-        href: "https://www.forumstandaardisatie.nl/node/229",
-        publisher: "Forum Standaardisatie",
+            title: "pas-toe-of-leg-uit",
+            href: "https://www.forumstandaardisatie.nl/node/229",
+            publisher: "Forum Standaardisatie",
         },
     },
 };
